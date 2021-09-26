28ee70364908218359dc1175be67dfb4154644ba
muthu <mkandanmk7@gmail.com>
2021-09-26 13:35:35 +0530

    hooks  counter done

--- src/Test.js
+++ src/Test.js
@@ -1,4 +1,5 @@
 import { CounterContext } from "./Context";
+import TestHooks from "./TestHooks";
 
 export default function Test() {
   return (
@@ -8,14 +9,17 @@ export default function Test() {
           console.log(context); // all values available here from app component;
 
           return (
-            <div className="App">
-              <hr />
-              <h2>Welcome to Test Component</h2>
-              <p>Count: {context.count}</p>
-              <button onClick={context.increment}>Test Inc</button>
-              <button onClick={context.decrement}>Test Dec</button>
-              <button onClick={context.reset}>Test Reset</button>
-            </div>
+            <>
+              <div className="App">
+                <hr />
+                <h2>Welcome to Test Component</h2>
+                <p>Count: {context.count}</p>
+                <button onClick={context.increment}>Test Inc</button>
+                <button onClick={context.decrement}>Test Dec</button>
+                <button onClick={context.reset}>Test Reset</button>
+              </div>
+              <TestHooks />
+            </>
           );
         }}
       </CounterContext.Consumer>
---
+++ src/TestHooks.js
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import { CounterContext } from "./Context";
+
+export default function TestHooks() {
+  const context = useContext(CounterContext);
+  console.log(context);
+  return (
+    <div className="App">
+      <h2>Test Hooks Component </h2>
+      <p>
+        <b>Count by Hooks : </b>
+        {context.count}
+      </p>
+      <button onClick={context.increment}>Hooks Inc</button>
+      <button onClick={context.decrement}>Hooks Dec</button>
+      <button onClick={context.reset}>Hooks Reset</button>
+    </div>
+  );
+}
